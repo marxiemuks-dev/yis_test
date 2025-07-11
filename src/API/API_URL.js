17931166a4867ccc16197e966222cd54be5abda5
Marxie <jhomsabdulmukim@gmail.com>
2025-07-11 19:48:35 +0800

    Update: API

--- src/API/API_URL.js
+++ src/API/API_URL.js
@@ -1,8 +1,8 @@
 //const API_URL = "http://127.0.0.1:5000/api/auth/";
 
 //LAN
-const API_URL = "http://192.168.1.171:5000/api/auth/"
+// const API_URL = "http://192.168.1.171:5000/api/auth/"
 
-
+const API_URL ="https://yis-test-server.onrender.com/"
 
 export default API_URL;
